78e80d4cf61e61bc032612f638007eff04903e4a
Olga Smirnova <almazkacode@gmail.com>
2025-03-31 16:29:30 +0300

    feat: add main component

---
+++ src/components/Main/Main.style.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  max-width: 1920px;
+  margin: 0 auto;
+  padding: 300px;
+`;
+
+export const CentralBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+  padding: 30px;
+  background-color: #e0e0e0;
+`;
+
+export const Title = styled.h1`
+  font-size: 100px;
+  font-weight: 100;
+  color: #d1a1a1;
+`;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import * as SC from './Main.style';
+
+export const Main: React.FC = () => {
+  return (
+    <SC.Container>
+      <SC.CentralBlock>
+        <SC.Title>todos</SC.Title>
+      </SC.CentralBlock>
+    </SC.Container>
+  );
+};
